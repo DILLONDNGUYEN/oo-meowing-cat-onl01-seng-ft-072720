b8c96f5c002d70289a6462d4b92be045acb8ce77
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-05 06:55:43 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -4,5 +4,14 @@ class Cat
   def name= (cat_name)
     @name= cat_name
   end
-  def
+  def name
+    @name
+  end
+  
+  def meow= (cat_meow)
+    @meow= cat_meow
+  end
+  def meow
+    @meow
+  end
 end
