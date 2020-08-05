9cbedd550d089b97a6944440bb7ced86f614662c
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-05 07:02:58 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,17 +1,26 @@
 ## code your solution here. 
 
 class Cat
-  def name= (cat_name)
-    @name= cat_name
-  end
-  def name
-    @name
-  end
   
-  def meow= (cat_meow)
-    @meow= cat_meow
-  end
-  def meow
-    @meow
-  end
-end
+
+
+
+
+
+
+
+#class Cat
+#  def name= (cat_name)
+#    @name= cat_name
+#  end
+#  def name
+#    @name
+#  end
+  
+#  def meow= (cat_meow)
+#    @meow= cat_meow
+#  end
+#  def meow
+#    @meow
+#  end
+#end
